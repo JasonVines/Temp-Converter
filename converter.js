7c88c0608f0b279800779f70acdcd29184e776f9
JasonVines <mrvines94@gmail.com>
2017-07-18 15:18:28 -0500

    Final Temp Converter docs

---
+++ converter.js
@@ -0,0 +1,47 @@
+"use strict";
+
+console.log("converter.js");
+console.log("test");
+// Temperature Converter
+
+
+// script for resetting the form
+let clearButton = document.getElementById("clearForm");
+
+clearButton.addEventListener("click", resetForm);
+
+function resetForm (clickEvent) {
+	document.getElementById("tempConverter").reset();
+}
+
+function toCelsius () {
+	let x = document.getElementById("enter").value;
+	let temp = Math.round((x - 32) * (5 / 9));
+	console.log(temp);
+	document.getElementById("display").value = temp;
+}
+
+function toFahrenheit () {
+	let x = document.getElementById("enter").value;
+	let temp = Math.round((x * (9 / 5)) + 32);
+	document.getElementById("display").value = temp;
+}
+
+
+// This function should determine which conversion should
+// happen based on which radio button is selected.
+function determineConverter (clickEvent) {
+	if (document.getElementById("celsius").checked === true) {
+		toCelsius();
+	} else if (document.getElementById("fahrenheit").checked === true) {
+		toFahrenheit();
+	} 
+}
+
+
+// Get a reference to the button element in the DOM
+let submitButton = document.getElementById("converter");
+
+
+// Assign a function to be executed when the button is clicked
+submitButton.addEventListener("click", determineConverter);
